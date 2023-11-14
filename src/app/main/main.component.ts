6f952a8c2dda0d52bb0bd0a5ee6c7d03cd8b4ae9
Brendon Rodrigues <rodriguesbrendon861@gmail.com>
2023-11-14 14:37:03 -0300

    feat: created main and product-banner

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductBannerComponent } from "../product-banner/product-banner.component";
+
+@Component({
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [CommonModule, ProductBannerComponent]
+})
+export class MainComponent {
+
+}
---
+++ src/app/product-banner/product-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-banner.component.html',
+  styleUrl: './product-banner.component.css'
+})
+export class ProductBannerComponent {
+}
